4037c1a21da0f7046a5e6d6d27b2c82d58ba7ed3
Paul Janicot <paul.janicot@artfx.fr>
2025-01-13 14:28:43 +0100

    Movement work

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -13,13 +13,17 @@ public class Movement : MonoBehaviour
     public float decceleration;
     public float velPower;
     public float frictionAmount;
+    public float jumpForce;
+    public float gravityDown;
+
+    private bool isGrounded = false;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (Input.GetKey(KeyCode.D))
         {
@@ -29,6 +33,27 @@ public class Movement : MonoBehaviour
         {
             moveInput = -1f;
         }
+        else
+        {
+            moveInput = 0f;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+        if (rb.velocity.y < 0.2 && !isGrounded)
+        {
+            rb.gravityScale = gravityDown;
+        }
+        else
+        {
+            rb.gravityScale = 1;
+        }
+    }
+
+    void FixedUpdate()
+    {
+           
         float targetSpeed = moveInput * speed;
         float speedDif = targetSpeed - rb.velocity.x;
         float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : decceleration;
@@ -40,6 +65,20 @@ public class Movement : MonoBehaviour
         amount *= Mathf.Sign(rb.velocity.x);
         rb.AddForce(Vector2.right * -amount, ForceMode2D.Impulse);
 
-        
+        //Jump
+       
+    }
+    private void Jump()
+    {
+        isGrounded = false;
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") && !isGrounded)
+        {
+            isGrounded = true;
+        }
     }
 }
